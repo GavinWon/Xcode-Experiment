51c06c59f2a3f9c2c7055812ee62ce274b368ce3
Gavin Wong <gavinmwong@gmail.com>
2020-02-07 10:13:56 -0800

    I AM Rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Gavin Wong on 6/19/19.
+//  Copyright © 2019 Gavin Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
